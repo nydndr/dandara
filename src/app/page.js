3b171d1b75ebbc93ceb048f7cd6be0bd3be6357d
nydndr <nicolydndr@gmail.com>
2025-10-15 20:59:35 -0300

    Hello World!

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,62 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="font-sans max-w-4xl my-24 mx-auto">
+      
+      <section className="space-y-4">
+        <h1 className="font-bold text-3xl md:text-6xl tracking-tighter md:leading-16">Dandara is a brazilian product designer far too comfortable with code and niche problems.</h1>
+        <h2 className="font-medium text-xl md:text-3xl md:leading-10 text-gray-700">Taking care of all <Link href="/" className="textmarker-effect-auto">POS&nbsp;design&nbsp;at&nbsp;Stone</Link>, mapping the field of design at <Link href="/" className="textmarker-effect-auto">Design Map</Link> and curating content at Amarelo Dandara.</h2>
+      </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <section className="md:flex md:grid-cols-2 justify-between my-12 font-medium space-y-12 md:space-x-12">
+        <div className="w-full">
+          <div className="flex justify-between border-b-3 border-dotted py-1.5">
+            <h3 className="font-mono uppercase font-bold text-3xl tracking-wide">Experience</h3>
+            <button  className="opacity-50 hover:text-(--foreground) hover:opacity-100 hover:bg-(--dandara) rounded-md px-4 mr-2 transition-all ease-in-out duration-300"><Link target="_blank" href="https://drive.google.com/file/d/1K8dhryLe11a8_9aTGQfGifM8EKzTH6_S/view?usp=sharing">Get my CV ⤓</Link></button>
+          </div>
+          <div className="py-4 space-y-6">
+          <article className="">
+            <p className="text-lg">Product Designer II @ <a className="underline underline-offset-3 " href="https://www.stone.com.br/">Stone Co.</a></p>
+            <p className="text-sm">2024 ━ Now</p>
+          </article>
+                    <article className="">
+            <p className="text-lg">Product Designer I @ <a className="underline underline-offset-3 " href="https://www.stone.com.br/">Stone Co.</a></p>
+            <p className="text-sm">2022 ━ 2024</p>
+          </article>
+                    <article className="">
+            <p className="text-lg">Product Designer I @ <a className="underline underline-offset-3 " href="https://grupoquintoandar.com/pt/">QuintoAndar</a></p>
+            <p className="text-sm">2021 ━ 2022</p>
+          </article>
+          </div>
+
+          <Link href="/career" className="float-right opacity-50 hover:text-(--foreground) hover:opacity-100 hover:bg-(--dandara) rounded-md px-4 py-2 transition-all ease-in-out duration-300">Read more about my career →</Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          
+        <div className="w-full">
+          <div className="flex justify-between border-b-3 border-dotted py-1.5">
+            <h3 className="font-mono uppercase font-bold text-3xl tracking-wide">Projects</h3>
+          </div>
+          <div className="py-4 space-y-6">
+          <article className="space-y-1">
+            <a className="text-2xl font-bold hover:underline hover:underline-offset-2">Design Map ↗</a>
+            <p className="w-10/12">The field of design and all its super specific subfields, visualized.</p>
+          </article>
+          <article className="space-y-1">
+            <Link target="_blank" href="https://nydndr.substack.com/"className="text-2xl font-bold hover:underline hover:underline-offset-2">Amarelo Dandara ↗</Link>
+            <p className="w-10/12">Monthly newsletter with the best I find around the web & world.</p>
+          </article>
+          <article className="space-y-1">
+            <Link target="_blank" href="https://www.behance.net/gallery/123564241/Fin-Financas-Jovem"className="text-2xl font-bold hover:underline hover:underline-offset-2">Fin. → <span className="text-lg font-mono uppercase text-gray-500/50 font-medium">Awarded design</span></Link>
+            <p className="w-10/12">Personal finance application for the new generation, #1 at UI/UX Jr. Competition. </p>
+          </article>
+          </div>
+
+          {
+            // <Link href="/career" className="float-right opacity-50 hover:text-(--foreground) hover:opacity-100 hover:bg-(--dandara) rounded-md px-4 py-2 transition-all ease-in-out duration-300">See all projects →</Link>
+          }
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/highlight.js
@@ -0,0 +1,3 @@
+export default function Highlight({children, content}) {
+  return <><p>{content}</p></>;
+}
